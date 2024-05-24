fc029dfdd500b0afd40b69e5f0c3ce78f5a259bd
tushar <tushardixit371@gmail.com>
2024-05-25 02:11:28 +0530

    initial commit

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<div className="">
+			<h1 className="text-red-500">Home</h1>
+      <Button>Click me</Button>
+		</div>
+	);
+}
