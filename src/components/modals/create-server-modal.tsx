6dea686053cde3e7eed62b2ad2651a8fe964ad35
tushar <tushardixit371@gmail.com>
2024-05-28 00:58:53 +0530

    feat: create server modal in sidebar

---
+++ src/components/modals/create-server-modal.tsx
@@ -0,0 +1,143 @@
+'use client';
+import FileUpload from '@/components/file-upload';
+import { Button } from '@/components/ui/button';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from '@/components/ui/dialog';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useModalStore } from '@/hooks/use-modal-store';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+const formSchema = z.object({
+	name: z
+		.string()
+		.min(3, {
+			message: 'Name must be at least 3 characters long',
+		})
+		.max(32),
+	imageUrl: z
+		.string()
+		.min(1, {
+			message: 'Server image is required',
+		})
+		.url({ message: 'Server image must be a valid URL' }),
+});
+
+const CreateServerModal = () => {
+	const {isOpen, onClose, type} = useModalStore();
+	const router = useRouter();
+	const isModalOpen = isOpen && type === 'createServer';
+	const form = useForm({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: '',
+			imageUrl: '',
+		},
+	});
+
+	const isLoading = form.formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		try {
+			await axios.post('/api/servers', values);
+			form.reset();
+			router.refresh();
+			onClose();
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const handleClose = () => {
+		form.reset();
+		onClose();
+	}
+
+	return (
+		<Dialog open={isModalOpen} onOpenChange={handleClose}>
+			<DialogContent className="bg-white text-black p-0 overflow-hidden">
+				<DialogHeader className="pt-8 px-6">
+					<DialogTitle className="text-2xl text-center font-bold">
+						Customize your server !
+					</DialogTitle>
+					<DialogDescription className="text-center">
+						Give your server a personality by adding a name and a profile
+						picture.You can always change this later.
+					</DialogDescription>
+				</DialogHeader>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+						<article className="space-y-8 px-6">
+							<article className="flex items-center justify-center text-center">
+								<FormField
+									control={form.control}
+									name="imageUrl"
+									render={({ field }) => (
+										<FormItem>
+											<FormControl>
+												<FileUpload
+													endpoint="serverImage"
+													value={field.value}
+													onChange={field.onChange}
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
+							</article>
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+											Server Name
+										</FormLabel>
+										<FormControl
+											className="bg-zinc-300/50 border-0 text-black 
+                                        focus-visible:ring-0
+                                        focus-visible:ring-offset-0
+                                        "
+										>
+											<Input
+												disabled={isLoading}
+												className=""
+												placeholder="Enter server name"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</article>
+						<DialogFooter className="bg-gray-100 px-6 py-4">
+							<Button variant="primary" disabled={isLoading}>
+								Create
+							</Button>
+						</DialogFooter>
+					</form>
+				</Form>
+			</DialogContent>
+		</Dialog>
+	);
+};
+
+export default CreateServerModal;
