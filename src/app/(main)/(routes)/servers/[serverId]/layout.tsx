0ee87ab8ce891b749569beb957723394912b42a3
tushar <tushardixit371@gmail.com>
2024-05-29 01:40:54 +0530

    feat: server side bar created and options for the server is added

---
+++ src/app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,43 @@
+import ServerSidebar from '@/components/server/server-sidebar';
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+async function ServerLayout({
+	children,
+	params,
+}: Readonly<{
+	children: React.ReactNode;
+	params: { serverId: string };
+}>) {
+	const profile = await currentProfile();
+	if (!profile) {
+		return auth().redirectToSignIn();
+	}
+	const server = await db.server.findUnique({
+		where: {
+			id: params.serverId,
+			members: {
+				some: {
+					profileId: profile.id,
+				},
+			},
+		},
+	});
+
+	if (!server) {
+		return redirect('/');
+	}
+
+	return (
+		<main className="h-full">
+			<article className="hidden md:flex h-full w-60 z-20 flex-col inset-y-0 fixed">
+                <ServerSidebar serverId={params.serverId}/>
+            </article>
+			<section className="h-full md:pl-60">{children}</section>
+		</main>
+	);
+}
+
+export default ServerLayout;
