a62d6f815c7ec9aedf12eb0ab978afcd13a28e8f
TusharDixit-301 <tushardixit371@gmail.com>
2024-06-03 02:11:03 +0530

    feat: change role and kick user functionality added

--- src/components/server/server-header.tsx
+++ src/components/server/server-header.tsx
@@ -56,7 +56,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+            onClick={() => onOpen('members', { server })}
+          >
             Manage Members
             <User className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
