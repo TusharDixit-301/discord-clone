5e4afab1838611ffb7fcd00b69f8a231101bb101
TusharDixit-301 <tushardixit371@gmail.com>
2024-06-10 01:43:34 +0530

    feat: edit channel functionality added

--- src/app/api/channels/[channelId]/route.ts
+++ src/app/api/channels/[channelId]/route.ts
@@ -54,3 +54,62 @@ export async function DELETE(
     return new NextResponse('Internal Error', { status: 500 });
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get('serverId');
+    const { name, type } = await req.json();
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse('Server ID missing', { status: 400 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse('Channel ID missing', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                not: 'general',
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return new NextResponse('Channel deleted successfully', { status: 200 });
+  } catch (error) {
+    console.log('[DELETE_CHANNEL]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
