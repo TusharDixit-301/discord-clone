b2cfc4e250c8ef39260b785ad5f4571c201f6227
TusharDixit-301 <tushardixit371@gmail.com>
2024-06-10 02:18:04 +0530

    fix: initial server page for redirecting to general channel

--- src/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,46 @@
-const ServesPage = ({ params }: { params: { serverId: string } }) => {
-  return <div>ServesPage: {params.serverId}</div>;
+import { currentProfile } from '@/lib/current-profile';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'general',
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+  if (!initialChannel) {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel.id}`);
 };
 
-export default ServesPage;
+export default ServerIdPage;
