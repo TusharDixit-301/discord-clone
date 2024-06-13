bbaef6b856c21d146bf1c7aca18f99f71e7160ff
TusharDixit-301 <tushardixit371@gmail.com>
2024-06-14 02:49:15 +0530

    fix: channel names and member names css

--- src/components/server/server-section.tsx
+++ src/components/server/server-section.tsx
@@ -23,7 +23,7 @@ const ServerSection = ({
   const { onOpen } = useModal();
   return (
     <div className="flex items-center justify-between py-2">
-      <p className="text-xs uppercase font-semibold text-zinc-500 dark:bg-zinc-400">
+      <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
         {label}
       </p>
       {role !== MemberRole.GUEST && sectionType === 'channels' && (
